3f8a186f3c61ee822d0a2df8fe1f12fdedc2f7a6
vulcan-action <vulcan@action>
2022-06-24 06:54:23 +0000

    Committed the automatically generated patch

--- src/BranchChecker.c
+++ src/BranchChecker.c
@@ -73,70 +73,43 @@ double* cohenSutherlandClip(double x1, double y1,
 
     for (int i = 0; i < 4; i++) {
 
-        if ((code1 == 0)) {  //Original: (code1 == 0) && (code2 == 0)
-            // If both endpoints lie within rectangle
-            printf("T code1: %d code2: %d\n", code1, code2);
-            accept = true;
-            break;
-        }
-        else if (code1 & code2) { //original: code1 & code2
-            printf("F code1: %d code2: %d\n", code1, code2);
-            // If both endpoints are outside rectangle,
-            // in same region
-            break;
-        }
-        else {
-            // Some segment of line lies within the
-            // rectangle
-            int code_out;
-            double x, y;
-
-            // At least one endpoint is outside the
-            // rectangle, pick it.
-            if (code1 != 0)
-                code_out = code1;
-            else
-                code_out = code2;
-
-            // Find intersection point;
-            // using formulas y = y1 + slope * (x - x1),
-            // x = x1 + (1 / slope) * (y - y1)
-            if (code_out & TOP) { //original: code_out & TOP
-                // point is above the clip rectangle
-                x = x1 + (x2 - x1) * (y_max - y1) / (y2 - y1);
-                y = y_max;
-            }
-            else if (code_out & BOTTOM) {
-                // point is below the rectangle
-                x = x1 + (x2 - x1) * (y_min - y1) / (y2 - y1);
-                y = y_min;
-            }
-            else if (code_out & RIGHT) {
-                // point is to the right of rectangle
-                y = y1 + (y2 - y1) * (x_max - x1) / (x2 - x1);
-                x = x_max;
-            }
-            else if (code_out & LEFT) {
-                // point is to the left of rectangle
-//                y = y1 + (y2 - y1) * (x_min - x1) / (x2 - x1);  // Original
-                y = (y2 - y1) * (x_min - x1) / (x2 - x1);
-                x = x_min;
-            }
-
-            // Now intersection point x, y is found
-            // We replace point outside rectangle
-            // by intersection point
-            if (code_out == code1) {
-                x1 = x;
-                y1 = y;
-                code1 = computeCode(x1, y1);
-            }
-            else {
-                x2 = x;
-                y2 = y;
-                code2 = computeCode(x2, y2);
-            }
-        }
+        if (((code1 == 0)) && !(code2 != 0)) {
+    printf("T code1: %d code2: %d\n", code1, code2);
+    accept = 1;
+    break;
+} else if (code1 & code2) {
+    printf("F code1: %d code2: %d\n", code1, code2);
+    break;
+} else {
+    int code_out;
+    double x, y;
+    if (code1 != 0)
+        code_out = code1;
+    else
+        code_out = code2;
+    if (code_out & TOP) {
+        x = x1 + (x2 - x1) * (y_max - y1) / (y2 - y1);
+        y = y_max;
+    } else if (code_out & BOTTOM) {
+        x = x1 + (x2 - x1) * (y_min - y1) / (y2 - y1);
+        y = y_min;
+    } else if (code_out & RIGHT) {
+        y = y1 + (y2 - y1) * (x_max - x1) / (x2 - x1);
+        x = x_max;
+    } else if (code_out & LEFT) {
+        y = (y2 - y1) * (x_min - x1) / (x2 - x1);
+        x = x_min;
+    }
+    if (code_out == code1) {
+        x1 = x;
+        y1 = y;
+        code1 = computeCode(x1, y1);
+    } else {
+        x2 = x;
+        y2 = y;
+        code2 = computeCode(x2, y2);
+    }
+}
     }
     clippedLine[0] = x1;
     clippedLine[1] = y1;
