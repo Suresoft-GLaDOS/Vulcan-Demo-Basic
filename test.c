ec51253a9b69e28d915680ed57d7a6921cc72896
BaeEB <ebbae@suresofttech.com>
2022-07-26 09:38:45 +0900

    Combine two way to excute the tests

--- test.c
+++ test.c
@@ -426,18 +426,7 @@ int main(int argc, char *argv[]) {
             for (int i = 0; i < 11; i++) {
                 root = Insert(root, input6_1[test_index][i]);
             }
-//            root = arrInsertDelete(root, input6_1[test_index][0]
-//                                  , input6_1[test_index][1]
-//                                  , input6_1[test_index][2]
-//                                  , input6_1[test_index][3]
-//                                  , input6_1[test_index][4]
-//                                  , input6_1[test_index][5]
-//                                  , input6_1[test_index][6]
-//                                  , input6_1[test_index][7]
-//                                  , input6_1[test_index][8]
-//                                  , input6_1[test_index][9]
-//                                  , input6_1[test_index][10]
-//            );
+
 
             for (int i = 0; i < 3; i++) {
                 root = Delete(root, input6_2[test_index][i]);
@@ -447,6 +436,22 @@ int main(int argc, char *argv[]) {
 //                                , input6_2[test_index][2]);
 
             int* actual_output6 = getInorder(root);
+
+            root = NULL;
+            root = arrInsertDelete(root, input6_1[test_index][0]
+                      , input6_1[test_index][1]
+                      , input6_1[test_index][2]
+                      , input6_1[test_index][3]
+                      , input6_1[test_index][4]
+                      , input6_1[test_index][5]
+                      , input6_1[test_index][6]
+                      , input6_1[test_index][7]
+                      , input6_1[test_index][8]
+                      , input6_1[test_index][9]
+                      , input6_1[test_index][10]
+            );
+
+            int* actual_output6_2 = getInorder(root);
 
             for (int i = 0; i < 11; i++) {
                 printf("%d, ", actual_output6[i]);
@@ -461,12 +466,24 @@ int main(int argc, char *argv[]) {
                     break;
                 }
             }
+
+            for (int i = 0; i < 11; i++) {
+                printf("Actual: %d  Expected: %d\n", actual_output6_2[i], expected_output6[test_index][i]);
+                if (actual_output6_2[i] != expected_output6[test_index][i]) {
+
+//                    compare = false;
+                    break;
+                }
+            }
+
             if (compare) {
                 printf("PASSED\n");
             }
             else {
                 printf("FAILED\n");
             }
+
+
             return compare == true ? 0 : 1;
 
 
