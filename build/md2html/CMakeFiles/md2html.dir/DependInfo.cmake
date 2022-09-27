7e82b08c819caa35ebb31b9d07b3988e37a26a62
BaeEB <ebbae@suresofttech.com>
2022-09-27 11:11:37 +0900

    Do not add Makefile on root directory

---
+++ build/md2html/CMakeFiles/md2html.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/sure/GLaDOS/Vulcan-Demo-Basic/md2html/cmdline.c" "md2html/CMakeFiles/md2html.dir/cmdline.c.o" "gcc" "md2html/CMakeFiles/md2html.dir/cmdline.c.o.d"
+  "/mnt/c/Users/sure/GLaDOS/Vulcan-Demo-Basic/md2html/entity.c" "md2html/CMakeFiles/md2html.dir/entity.c.o" "gcc" "md2html/CMakeFiles/md2html.dir/entity.c.o.d"
+  "/mnt/c/Users/sure/GLaDOS/Vulcan-Demo-Basic/md2html/md2html.c" "md2html/CMakeFiles/md2html.dir/md2html.c.o" "gcc" "md2html/CMakeFiles/md2html.dir/md2html.c.o.d"
+  "/mnt/c/Users/sure/GLaDOS/Vulcan-Demo-Basic/md2html/render_html.c" "md2html/CMakeFiles/md2html.dir/render_html.c.o" "gcc" "md2html/CMakeFiles/md2html.dir/render_html.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/sure/GLaDOS/Vulcan-Demo-Basic/build/md4c/CMakeFiles/md4c.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
