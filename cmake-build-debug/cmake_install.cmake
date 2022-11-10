74ba636d64a88e7c7c930276255eafd8ca0c3ad0
BaeEB <ebbae@suresofttech.com>
2022-11-10 10:18:44 +0900

    Refine test commands

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -37,11 +37,94 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.1.3/bin/mingw/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/md4c/cmake_install.cmake")
-  include("C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/md2html/cmake_install.cmake")
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/MD4C/lib/libchewing/dict.dat;C:/Program Files (x86)/MD4C/lib/libchewing/fonetree.dat;C:/Program Files (x86)/MD4C/lib/libchewing/ph_index.dat;C:/Program Files (x86)/MD4C/lib/libchewing/us_freq.dat;C:/Program Files (x86)/MD4C/lib/libchewing/ch_index_begin.dat;C:/Program Files (x86)/MD4C/lib/libchewing/ch_index_phone.dat")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/MD4C/lib/libchewing" TYPE FILE FILES
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/data/dict.dat"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/data/fonetree.dat"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/data/ph_index.dat"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/data/us_freq.dat"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/data/ch_index_begin.dat"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/data/ch_index_phone.dat"
+    )
+endif()
 
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/MD4C/include/chewing/chewing.h;C:/Program Files (x86)/MD4C/include/chewing/chewingio.h;C:/Program Files (x86)/MD4C/include/chewing/global.h;C:/Program Files (x86)/MD4C/include/chewing/mod_aux.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/MD4C/include/chewing" TYPE FILE FILES
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/include/chewing.h"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/include/chewingio.h"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/include/global.h"
+    "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/include/mod_aux.h"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/MD4C/lib/pkgconfig/chewing.pc")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/MD4C/lib/pkgconfig" TYPE FILE FILES "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/chewing.pc")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/MD4C/lib/libchewing.dll.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/MD4C/lib" TYPE STATIC_LIBRARY OPTIONAL FILES "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/libchewing.dll.a")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/MD4C/lib/libchewing.dll")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/MD4C/lib" TYPE SHARED_LIBRARY FILES "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/libchewing.dll")
+  if(EXISTS "$ENV{DESTDIR}/C:/Program Files (x86)/MD4C/lib/libchewing.dll" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/C:/Program Files (x86)/MD4C/lib/libchewing.dll")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "C:/Program Files/JetBrains/CLion 2022.1.3/bin/mingw/bin/strip.exe" "$ENV{DESTDIR}/C:/Program Files (x86)/MD4C/lib/libchewing.dll")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "C:/Program Files (x86)/MD4C/lib/libchewing.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "C:/Program Files (x86)/MD4C/lib" TYPE STATIC_LIBRARY FILES "C:/Users/sure/GLaDOS/Vulcan-Demo-Basic/cmake-build-debug/libchewing.a")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
